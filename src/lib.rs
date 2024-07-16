aec5eec3c111dc15693d5d2164a7e7c20909f57d
Nick Stone <nicholasestone@proton.me>
2024-07-16 16:39:15 -0500

    Updated to use frame::DynamicImage to support a bunch of different image encodings

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,21 @@
-use core::ops::Deref;
-use image::{ImageBuffer, Pixel};
+use image::DynamicImage;
 use std::sync::mpsc::Sender;
 use std::time::Duration;
 
 ///Struct to represent a single frame. `P` and `C` are type parameters of the underlying
 ///[ImageBuffer]
-pub struct Frame<P: Pixel, C: Deref<Target = [P::Subpixel]>> {
+pub struct Frame {
     timestamp: Duration,
-    image: ImageBuffer<P, C>,
+    image: DynamicImage,
 }
-impl<P: Pixel, C: Deref<Target = [P::Subpixel]>> Frame<P, C> {
+impl Frame {
     ///Create a new `Frame`
-    pub fn new(timestamp: Duration, image: ImageBuffer<P, C>) -> Frame<P, C> {
-        Frame::<P, C> { timestamp, image }
+    pub fn new(timestamp: Duration, image: DynamicImage) -> Frame {
+        Frame { timestamp, image }
     }
     ///Get the image associated with this `Frame`. This method will consume the `Frame`
     ///struct to avoid copying image data
-    pub fn to_image(self) -> ImageBuffer<P, C> {
+    pub fn to_image(self) -> DynamicImage {
         self.image
     }
     ///Get the image associated with this `Frame`
@@ -27,10 +26,6 @@ impl<P: Pixel, C: Deref<Target = [P::Subpixel]>> Frame<P, C> {
 
 ///A source of frames (such as a camera)
 pub trait FrameSource {
-    ///[Pixel] type of the frames which will be returned
-    type PixelType: Pixel;
-    ///Underlying container holding the pixels which make up a frame
-    type ImageContainerType: Deref<Target = [<Self::PixelType as Pixel>::Subpixel]>;
     ///The [FrameStream] that will stream frames from this source
     type Stream: FrameStream;
     ///Set the exposure time
@@ -42,25 +37,13 @@ pub trait FrameSource {
     ///Get the resolution
     fn get_resolution(&self) -> [usize; 2];
     ///Start streaming frames. The frames will be placed into the provided [Sender].
-    fn start(
-        &self,
-        sender: Sender<Frame<Self::PixelType, Self::ImageContainerType>>,
-    ) -> Self::Stream;
+    fn start(&self, sender: Sender<Frame>) -> Self::Stream;
 }
 
 ///A running stream of frames
 pub trait FrameStream {
-    ///[Pixel] type of the frames
-    type PixelType: Pixel;
-    ///Underlying container holding the pixel data
-    type ImageContainerType: Deref<Target = [<Self::PixelType as Pixel>::Subpixel]>;
     ///Stop streaming
     fn stop(self);
     ///Change the destination of the frame stream. New frames will be placed into the provided [Sender]
-    fn change_consumer(
-        &mut self,
-        sender: Sender<
-            Frame<<Self as FrameStream>::PixelType, <Self as FrameStream>::ImageContainerType>,
-        >,
-    );
+    fn change_consumer(&mut self, sender: Sender<Frame>);
 }
